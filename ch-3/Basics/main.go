a5ff4a3ae0132ce95a4ef1b35d5c775e40586520
Abhishek Verma <ak8888780@gmail.com>
2024-02-05 23:10:10 +0530

    done till switch here

--- ch-3/Basics/main.go
+++ ch-3/Basics/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 // constant
 const language string = "Go"
@@ -100,5 +104,138 @@ func main() {
 	b := []byte(s)
 	fmt.Printf("%7T %v\n", s, s) // string foo
 	fmt.Printf("%7T %v\n", r, r) // []int32 [102 111 246]
-	fmt.Printf("%7T %v\n", b, b) // []uint8 [102 111 195 182]
+	fmt.Printf("%7T %v\n", b, b)
+
+	// Maps
+	fmt.Println("MAPS:")
+	mapp := make(map[string]int)
+	mapp["abhi"] = 71
+	mapp["kd"] = 69
+	fmt.Println(mapp)
+	delete(mapp, "kd")
+	fmt.Println(mapp)
+
+	// initialize and populated as map literals
+	mapps := map[string]int{
+		"abhi":  71,
+		"kd":    22,
+		"rajat": 69,
+	}
+	fmt.Println(mapps)
+
+	///maps membership testing
+	foo := mapps["sezal"]
+	fmt.Println(foo)
+	newton, ok := mapps["sezal"]
+	fmt.Println(ok)
+	fmt.Println(newton)
+
+	// Pointers
+	fmt.Println("POINTERS: ")
+
+	var ar int = 71
+	var p *int = &ar
+	fmt.Println(p)
+	fmt.Println("Value of p: ", *p)
+	*p = 69
+	fmt.Println("Value of ar: ", ar)
+
+	// One loop only -> for Loop
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += 1
+	}
+	fmt.Println(sum)
+
+	summ, i := 0, 0
+	for i < 10 { // Equivalent to: for; i < 10;
+		summ += i
+		i++
+	}
+	fmt.Println(i, summ) // 10 45
+
+	// range over for loop
+	abhii := []int{2, 4, 8, 16, 32}
+	for val, ind := range abhii {
+		fmt.Println(val, "->", ind)
+	}
+
+	abhii2 := []int{2, 4, 6, 8, 10}
+	for _, vall := range abhii2 { // ommiting the index
+		fmt.Println(vall)
+	}
+
+	aa := []int{0, 2, 4, 6, 8}
+	summm := 0
+	for _, v := range aa {
+		summm += v
+	}
+	fmt.Println(summm)
+
+	// loop over maps
+	mmap := map[int]string{
+		1: "jan",
+		2: "feb",
+		3: "march",
+	}
+	for k, v := range mmap {
+		fmt.Println(k, "->", v)
+	}
+
+	// IF ELSE
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if _, err := os.Open("main.go"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("All is well")
+	}
+
+	_, er := os.Open("main.go")
+	if er != nil {
+		fmt.Println("err")
+	} else {
+		fmt.Println("All is well")
+	}
+
+	// SWITCH
+	varr := 0
+	switch varr % 3 {
+	case 0:
+		fmt.Println("Zero")
+		fallthrough
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	default:
+		fmt.Println("huh")
+	}
+
+	hour := time.Now().Hour()
+
+	switch {
+	case hour >= 5 && hour < 9:
+		fmt.Println("I am working")
+	case hour >= 9 && hour < 18:
+		fmt.Println("I am writing")
+	default:
+		fmt.Println("I am sleeping")
+	}
+
+	// same but different approach
+	switch hour := time.Now().Hour(); {
+	case hour >= 5 && hour < 9:
+		fmt.Println("I am writing")
+	case hour >= 9 && hour < 18:
+		fmt.Println("I am working")
+	default:
+		fmt.Println("I am sleeping bruhh ")
+
+	}
 }
