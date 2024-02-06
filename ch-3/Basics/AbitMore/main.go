6370c6f88b9d026407625f089bd46de96325c595
Abhishek Verma <ak8888780@gmail.com>
2024-02-06 10:51:55 +0530

    till defer

---
+++ ch-3/Basics/AbitMore/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type error interface {
+	Error() string
+}
+
+func main() {
+	// file, err := os.Open("main.go")
+	//	if err != nil {
+	//		log.Fatal(err)
+	//		return err
+	//	}
+
+	//	e1 := errors.New("error 71 ")
+	//	e2 := fmt.Errorf("error %d", 71)
+
+	//type NestedError struct {
+	//	Message string
+	//Err     error
+	//}
+
+	//func (e *NestedError) Error() string {
+	//return fmt.Sprintf("%s\n contains: %s", e.Message, e.Err.Error())
+	//}
+
+	sum := add(10, 61)
+	fmt.Println(sum)
+
+	a, b := swap("foo", "bar")
+	fmt.Println(a, b)
+
+	defer fmt.Println("Executed after below line")
+	fmt.Println("Executed before defer")
+}
+
+// FUNCTIONS
+func add(x, y int) int {
+	return x + y
+}
+
+// MULTIPLE RETURN
+func swap(a, b string) (string, string) {
+	return b, a
+}
